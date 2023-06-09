e8d00c1d8fbb4c1d7486c3387e46cfc1dc64897f
Valdir Silva <valdir.ti@gmail.com>
2023-06-09 15:19:37 -0300

    feat: first commit and hero section

---
+++ components/Hero.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+import Image from "next/image"
+import CustomButton from "./CustomButton"
+
+const Hero = () => {
+
+    const handleScroll = () => {}
+
+  return (
+    <div className="hero">
+        <div className="flex-1 pt-36 padding-x">
+
+            <h1 className="hero__title">
+                Encontre, agende ou alugue um carro -- rápido e fácil
+            </h1>
+
+            <p className="hero__subtitle">
+                Simplifique sua experiência de aluguel de carros com nosso processo de reserva sem esforço
+            </p>
+
+            <CustomButton
+                title="Explore"
+                containerStyles="bg-primary-blue text-white rounded-full mt-10"
+                handleClick={handleScroll}
+            />
+
+        </div>
+
+        <div className="hero__image-container">
+            <div className="hero__image">
+                <Image src="/hero.png" alt="hero" fill className="object-contain" />
+                <div className="hero__image-overlay" />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
